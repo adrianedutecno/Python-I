331daa63003a011497eb855fe2bb579eb823c0c6
afranco <adrian.franco@edutecno.com>
2023-04-20 19:47:56 -0400

    feature-S4: variables

---
+++ 4-S4/variables.py
@@ -0,0 +1,21 @@
+#tipos de datos
+#int
+#str
+#float
+#bool
+
+#declaracion de variables
+# identicador = valor
+numero = 10 #variable con valor asignado entero
+
+#las variables en python se reasignan y pueden cambiar su valor a diferentes tipos de datos
+numero = "cadena de texto o caracteres"
+numero = True
+
+texto = "cadena de caracteres"
+decimal = 1.5 
+condicional = True
+condicional = False
+
+#metodo de impresion
+print(numero)
