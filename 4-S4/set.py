06598305e0cba3c91036bbaa77e8ae30c6044b07
afranco <adrian.franco@edutecno.com>
2023-04-20 21:46:59 -0400

    feature-S4: set y tupla

---
+++ 4-S4/set.py
@@ -0,0 +1,18 @@
+#set, coleccion de elementos y son unicos, no acepta valores repetidos
+lista = [1,2,3] #lista
+lista = set(lista) #declarando un set(elemento) set({1,2,3})
+print(lista)
+nombre_set = set({1,2,3,4,5,6,7,8,9,10,11})
+set = {1,1,2,3}
+
+print(set) #{1, 2, 3}
+
+#metodos de set
+
+#añadir .add(elemento)
+set.add(4)
+print(set)
+
+#remover .remove(elemento)
+set.remove(1)
+print(set)
