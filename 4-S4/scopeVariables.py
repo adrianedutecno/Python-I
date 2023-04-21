261c27a0237656c8a5dfb1acdf582bcd5ff6e16f
afranco <adrian.franco@edutecno.com>
2023-04-20 20:01:05 -0400

    feature-S4: scope variables

---
+++ 4-S4/scopeVariables.py
@@ -0,0 +1,19 @@
+#scope o alcance de una variable
+
+#alcance global
+#alcance local
+
+#variable de alcance global porque no esta dentro de alguna estructura
+variable_global = "se puede acceder desde todo el documento"
+
+
+#metodo o funcion para ejemplicar una variable local
+#def nombre_metodo(parametros_entrada):
+def suma(a,b):
+    suma_valores = a+b #variable local porque existe solo dentro de la estructura de la funcion o metodo
+    #variable_global = suma_valores #asignando suma a la variable global 
+    return suma_valores
+
+print(suma(2,1)) #3
+print(variable_global)
+#print(suma_valores)
