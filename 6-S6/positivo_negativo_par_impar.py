2eca336cf394377ec25f30a1a9b16a30ff82c70b
afranco <adrian.franco@edutecno.com>
2023-04-25 22:36:57 -0400

    S6: condicionales

---
+++ 6-S6/positivo_negativo_par_impar.py
@@ -0,0 +1,26 @@
+"""
+indique si es positivo o negativo, y si es par o impar. 
+En caso de ser cero, también indicarlo.
+"""
+
+# Pedimos al usuario que introduzca un número
+num = int(input("Introduce un número: "))
+
+# Definimos un diccionario para asociar los valores pares y positivos a las etiquetas de salida
+output_labels = {
+    (0, 0): "El número es cero",
+    (1, 0): "El número es positivo y par",
+    (1, 1): "El número es positivo e impar",
+    (-1, 0): "El número es negativo y par",
+    (-1, 1): "El número es negativo e impar"
+}
+
+# Obtenemos los valores, si es par y si es positivo utilizando operaciones bit a bit
+parity = num & 1  # 1 si es impar, 0 si es par
+positivity = (num > 0) - (num < 0)  # 1 si es positivo, -1 si es negativo, 0 si es cero
+
+# Obtenemos la etiqueta de salida del diccionario utilizando los valores de paridad y positividad
+output_label = output_labels[(positivity, parity)]
+
+# Imprimimos la etiqueta de salida
+print(output_label)
--- 6-S6/rebound/rebound.py
+++ 6-S6/rebound/rebound.py
@@ -4,3 +4,18 @@ En caso de ser cero, también indicarlo.
 Se debe usar la expresión if-elif-else, y no usar subcondiciones; 
 en su lugar, usar condiciones anidadas.
 """
+
+num = int(input("Introduce un número: "))
+
+if num == 0:
+    print("El número es cero")
+elif num > 0:
+    if num % 2 == 0:
+        print("El número es positivo y par")
+    else:
+        print("El número es positivo e impar")
+else:
+    if num % 2 == 0:
+        print("El número es negativo y par")
+    else:
+        print("El número es negativo e impar")
