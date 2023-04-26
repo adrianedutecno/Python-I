ac87813acd3af0cb50760ac9d9864fb3368d7aaf
afranco <adrian.franco@edutecno.com>
2023-04-25 23:17:33 -0400

    S6: condicionales

--- 6-S6/positivo_negativo_par_impar.py
+++ 6-S6/positivo_negativo_par_impar.py
@@ -3,10 +3,11 @@ indique si es positivo o negativo, y si es par o impar.
 En caso de ser cero, también indicarlo.
 """
 
-# Pedimos al usuario que introduzca un número
+#Pedimos al usuario que introduzca un número
 num = int(input("Introduce un número: "))
 
-# Definimos un diccionario para asociar los valores pares y positivos a las etiquetas de salida
+#Definimos un diccionario para asociar los valores pares y positivos a las etiquetas de salida
+#asigna cada tupla de pares y positivos a una etiqueta que describe el número.
 output_labels = {
     (0, 0): "El número es cero",
     (1, 0): "El número es positivo y par",
@@ -15,11 +16,11 @@ output_labels = {
     (-1, 1): "El número es negativo e impar"
 }
 
-# Obtenemos los valores, si es par y si es positivo utilizando operaciones bit a bit
+#Obtenemos los valores, si es par y si es positivo utilizando operaciones bit a bit
 parity = num & 1  # 1 si es impar, 0 si es par
 positivity = (num > 0) - (num < 0)  # 1 si es positivo, -1 si es negativo, 0 si es cero
 
-# Obtenemos la etiqueta de salida del diccionario utilizando los valores de paridad y positividad
+#Obtenemos la etiqueta de salida del diccionario utilizando los valores pares y positivos
 output_label = output_labels[(positivity, parity)]
 
 # Imprimimos la etiqueta de salida
