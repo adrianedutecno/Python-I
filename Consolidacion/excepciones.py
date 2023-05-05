eaf213c03d43778daa7e635d5253839ca86de271
afranco <adrian.franco@edutecno.com>
2023-05-04 21:52:59 -0400

    S9: funciones

---
+++ Consolidacion/excepciones.py
@@ -0,0 +1,12 @@
+# Exception: Clase base para todas las excepciones definidas en Python.
+# AttributeError: Se produce cuando un objeto no tiene un atributo válido.
+# TypeError: Se produce cuando se utiliza un tipo de dato incorrecto o inapropiado.
+# ValueError: Se produce cuando se pasa un argumento con un valor inapropiado.
+# NameError: Se produce cuando se utiliza un nombre que no está definido en el contexto actual.
+# IndexError: Se produce cuando se intenta acceder a un índice de lista fuera de rango.
+# KeyError: Se produce cuando se intenta acceder a una clave de diccionario que no existe.
+# ZeroDivisionError: Se produce cuando se intenta dividir un número por cero.
+# FileNotFoundError: Se produce cuando un archivo no puede ser encontrado.
+# IOError: Se produce cuando hay un problema de entrada/salida en un archivo o en la consola.
+# KeyboardInterrupt: Se produce cuando el usuario interrumpe la ejecución del programa (presionando Ctrl + C en la consola).
+# StopIteration: Se produce cuando el método next() de un iterador no tiene más elementos para devolver.
